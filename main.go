69c837eb07f71db6b27a8158c312aa2dcf8a890f
Agam Ben Itzhak <116896541+AgamBenItzhak@users.noreply.github.com>
2024-03-30 13:41:56 +0000

    Import additional required packages to main

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/AgamBenItzhak/TaskTracker/cmd"
+import (
+	"github.com/AgamBenItzhak/TaskTracker/cmd"
+	_ "github.com/AgamBenItzhak/TaskTracker/config"
+	_ "github.com/AgamBenItzhak/TaskTracker/logger"
+)
 
 func main() {
 	cmd.Execute()
