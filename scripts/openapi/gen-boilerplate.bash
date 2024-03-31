14b329a0b6c2ea65147158d6226132ad0afff2f3
Agam Ben Itzhak <116896541+AgamBenItzhak@users.noreply.github.com>
2024-03-31 11:19:50 +0000

    Update code-gen-configs and generation script. Regenerated boilerplate

--- scripts/openapi/gen-boilerplate.bash
+++ scripts/openapi/gen-boilerplate.bash
@@ -13,7 +13,11 @@ echo "Generating code for the schema..."
 oapi-codegen -config api/code-gen-configs/schema.yaml api/openapi/schema.yaml
 
 echo "Generating code for the server..."
+
+echo "Generating code for the server: User Service..."
 oapi-codegen -config api/code-gen-configs/server/user.yaml api/openapi/server/user.yaml
+
+echo "Generating code for the server: Project Service..."
 oapi-codegen -config api/code-gen-configs/server/project.yaml api/openapi/server/project.yaml
 
 echo "files generated successfully"
